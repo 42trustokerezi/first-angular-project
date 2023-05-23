4059a854468e042b321d42c3add88e4fd0f84ccf
42trustokerezi <trustokerezi42@gmail.com>
2023-05-23 22:45:48 +0100

    interface created

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { Housinglocation } from '../housinglocation';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    HousingLocationComponent,
+  ],
   template: `
     <section>
       <form>
@@ -12,9 +17,21 @@ import { CommonModule } from '@angular/common';
         <button class="primary" type="button">Search</button>
       </form>
     </section>
+    <section class="results">
+    <app-housing-location></app-housing-location>
+    </section>
   `,
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-
+  housingLocation: Housinglocation = {
+    id: 9999,
+    name: 'Test Home',
+    city: 'Test city',
+    state: 'ST',
+    photo: 'assets/example-house.jpg',
+    availableUnits: 90,
+    wifi: true,
+    laundry: false,
+  }
 }
